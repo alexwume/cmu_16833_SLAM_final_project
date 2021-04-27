4c434e8be51186b76b69ad8aa4cb9d2d1d4d80bb
Alex <chichiaw@andrew.cmu.edu>
2021-04-27 11:02:31 -0400

    laser map added

--- info_subscriber.py
+++ info_subscriber.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
+from __future__ import division
 import rospy
 import numpy as np
 from std_msgs.msg import String
-from sensor_msgs.msg import LaserScan PointCloud2, PointField
+from sensor_msgs.msg import LaserScan
 from sensor_msgs import point_cloud2
 from geometry_msgs.msg import Pose
 from nav_msgs.msg import MapMetaData, OccupancyGrid
@@ -12,7 +13,7 @@ import matplotlib as mpl
 from pyquaternion import Quaternion
 from gazebo_msgs.msg import ModelStates 
 
-from __future__ import division
+
 '''
 author: Alex Wu
 email: chichiaw@andrew.cmu.edu
@@ -26,77 +27,104 @@ class map_reconstructor(object):
         \param: laser_reading: 360 laser readings
         '''
         self.robot_pose = robot_pose #[x,y,z, qx,qy,qz,qw] (7,)
+        
         self.laser_data = laser_data #360 laser readings (360, )
-        self.interval = 10
         self.map = curr_map
+        self.map_resolution = 0.05
+        self.map_origin = np.array([-10, -10, 0])
         self.laser_map = -1 * np.ones((384, 384))
 
 
-    def visualize_laser(self):
-        '''
-        visualize the laser reading (360 x 3)
-        '''
-        # print(self.robot_pose)
-        # print(len(self.laser_data))
-
-        # require pyquaternion
-
-        # read euler data for quaternion
-        robot_qw = self.robot_pose[6]
-        robot_qaxis = self.robot_pose[3:6]
-        robot_q = np.vstack((robot_qw, robot_qaxis))
-
-        # create quaternion
-        curr_quater = Quaternion(robot_q)
-        # retrieve theta from quaternion
-        theta = curr_quater.angle
-
-        # calculate laser readning
-        robot_x = self.robot_pose[0]
-        robot_y = self.robot_pose[1]
-        robot_z = self.robot_pose[2]    
-        laser_x = robot_x + laser_data * np.cos(theta)
-        laser_y = robot_y + laser_data * np.sin(theta)
-        laser_z = robot_z * np.ones(360)
-        laser_reading = np.vstack((laser_x, laser_y, laser_z)) # (3,360)
-        laser_reading = laser_reading.T #(360,3)
-        
-
-        # return laser_reading
-    def laser_map(self):
+    def create_laser_map(self):
         '''
         constuct a laser result map, where detected points will be 1, unknown space is -1 and free space is 0
         '''
+        if len(self.laser_data) == 0:
+            return
+
+        # create quaternion
+        curr_quater = Quaternion(self.robot_pose[6], self.robot_pose[3], self.robot_pose[4], self.robot_pose[5])
+        # retrieve theta from quaternion
+        theta = curr_quater.angle * 180 / np.pi # convert it into degree to do increment later
+
+        # print("theta", theta * 180 / np.pi )
+
+        ang = np.arange(360) + theta# 0 ~ 359 degree
+        ang = ang * np.pi / 180 # in radian
+        # print(len(self.laser_data * np.cos(ang)))
+       
+
+        for i in range(360):
+            cur_dist = 0
+            laser_dist = self.laser_data[i]
+            while cur_dist < laser_dist:
+                lx = self.robot_pose[0] +  cur_dist * np.cos((theta + i) * np.pi / 180) - self.map_origin[0] # 360 x 1
+                ly = self.robot_pose[1] +  cur_dist * np.sin((theta + i) * np.pi / 180) - self.map_origin[1]# 360 x 1
+
+                i_x = (lx / self.map_resolution).astype(int)
+                i_y = (ly / self.map_resolution).astype(int)
+                self.laser_map[i_x, i_y] = 0
+                cur_dist = cur_dist + self.map_resolution
 
         
+        laser_x = self.robot_pose[0] +  self.laser_data * np.cos(ang) # 360 x 1
+        laser_y = self.robot_pose[1] +  self.laser_data * np.sin(ang) # 360 x 1
 
-    # def map_reform(self):
-    #     '''
-    #     reconstruct a map including original map and laser readings
-    #     '''
-    #     # pass
-    #     pixel_value = 80 # pixel value for overlay
+        # visualize the laser results, this is only for testing purposes
+        self.laser_visualization_rviz(laser_x, laser_y)
 
-    #     # convert laser reading to axis(int)
-    #     laser_reading = visualize_laser(self)
-    #     laser_axis = np.rint(laser_reading)
-    #     laser_axis = laser_axis.astype(int)
-    #     new_map = np.zeros_like(self.map)
-    #     # get axes
-    #     overlay_x = laser_axis[0,:]
-    #     overlay_y = laser_axis[1,:]
-    #     # use numpy advanced indexing
-    #     new_map[overlay_x, overlay_y] = pixel_value
-    #     self.map += new_map
+        laser_x -= self.map_origin[0]
+        laser_y -= self.map_origin[1]
 
-    # def map_saver(self):
-    #     '''
-    #     save the map at a set interval
-    #     '''
-    #     curr_map = self.map
-    #     outfile = '../data/map_time_' + str(self.interval)
-    #     np.save(outfile, curr_map)
+        ind_x = (laser_x / self.map_resolution).astype(int)
+        ind_y = (laser_y / self.map_resolution).astype(int)
 
+        # set the detected points to 1
+        self.laser_map[ind_x, ind_y] = 1
+        # visualize the laser_map for testing purposes
+        # self.laser_map_visual()
+
+
+    def laser_map_visual(self):
+        # tell imshow about color map so that only set colors are used
+        pyplot.close()
+        cmap = mpl.colors.ListedColormap(['grey', 'white','black'])
+        bounds=[-2,0,1,10]
+        norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
+        robot_xind = int((self.robot_pose[0] - self.map_origin[0]) / self.map_resolution)
+        robot_yind = int((self.robot_pose[1] - self.map_origin[1]) / self.map_resolution)
+        self.laser_map[robot_xind, robot_yind] = 1
+        
+        img = pyplot.imshow(self.laser_map,cmap = cmap,norm=norm)
+        pyplot.show(block=False)
+        
+
+    def laser_visualization_rviz(self, x_data, y_data):
+        
+        topic = 'visualization_marker_array'
+        publisher = rospy.Publisher(topic, MarkerArray, queue_size=360)
+
+        markerArray = MarkerArray()
+
+        for count in range(360):
+            marker = Marker()
+            marker.header.frame_id = "/map"
+            marker.type = marker.SPHERE
+            marker.action = marker.ADD
+            marker.scale.x = 1.0
+            marker.scale.y = 1.0
+            marker.scale.z = 1.0
+            marker.color.a = 1.0
+            marker.color.r = 1.0
+            marker.color.g = 1.0
+            marker.color.b = 0.0
+            marker.pose.orientation.w = 1.0
+            marker.pose.position.x = x_data[count]
+            marker.pose.position.y = y_data[count]
+            marker.pose.position.z = 1
+            markerArray.markers.append(marker)
+            count += 1
+            publisher.publish(markerArray)
 
 
 class SD_algo(object):
@@ -220,7 +248,10 @@ class info_sub(object):
         Read in laser reading results from topic /scan
         '''
         max_reading = msg.range_max
-        self.detection = msg.ranges
+        self.detection = np.array(msg.ranges)
+        mask = self.detection < max_reading
+        self.detection = self.detection * mask
+        self.detection = np.nan_to_num(self.detection) + ~mask * max_reading
 
     def map_info(self, info):
         '''
@@ -238,13 +269,9 @@ class info_sub(object):
     def map_output(self, map):
         pyplot.close()
         # print(np.array(map.data).shape)
-        self.map = np.array(map.data)
-        print(max(self.map))
+        self.map = np.array(map.data).reshape((self.map_height, self.map_width))
         lv = map_reconstructor(self.robot_pose, self.detection, self.map) 
-        lv.visualize_laser()
-        lv.map_reform()
-        lv.map_saver()
-        # self.map_visualization()
+        lv.create_laser_map()
 
     def map_visualization(self):
         # tell imshow about color map so that only set colors are used
